71ebc930d4b909df6a05b17b66aac12a9e587f62
Lucas Melo <lmelo072002@gmail.com>
2023-04-13 18:06:49 -0300

    Form cadastro Estrutura

--- src/pages/Cadastrar/cadastrar.jsx
+++ src/pages/Cadastrar/cadastrar.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 import './cadastrar.css'
 
 const Cadastrar = () => {
     return ( 
-        <div className='teste'>
+        <div className='container-form-cadastro'>
+            <form className='form-cadastro' action="" method="post">
+                <h2 className='title' >Cadastrar</h2>
+                <div className='inputs-container'>
+                    <input className='input-form' type="text" name="" id="" placeholder='Nome' required />
+                    <input className='input-form' type="password" name="" id="" placeholder='Senha' required />
+                    <input className='input-form' type="password" name="" id="" placeholder='Confirmar Senha' required />
+                    <button className='button-form'>Cadastrar</button>
+                </div>    
 
+                <div className='container-opts'>
+                    <p>Administrador <Link className='link' to="">Criar Conta Adm</Link> </p>
+                    <p>Imprensario <Link className='link' to="">Criar Conta imprensario</Link> </p>
+                </div>
+            </form>
+        
         </div> 
     );
 }
